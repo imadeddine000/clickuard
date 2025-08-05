badde374d50b0d193967e7bb487d3514d5619fb3
imadeddine kebour <imadeddinekebour@gmail.com>
2025-08-05 13:38:32 +0100

    axios wrapper

--- app/api/agents/route.ts
+++ app/api/agents/route.ts
@@ -2,22 +2,19 @@ import axios from "axios";
 import { cookies, headers } from "next/headers";
 import { NextResponse } from "next/server";
 import https from 'https'
+import Axios from "@/lib/Axios";
 export async function GET() {
   try {
     const cookieStore = cookies();
     const token = (await cookieStore).get("token")?.value;
     console.log("TOKEN ISJ: ", token);
     
-    const response = await axios.get("https://101.44.39.177:55000/agents", {
-      headers: {
-        Authorization: `Bearer ${token}`,
+    
+    const response = await Axios.get("/agents",{
+       headers: {
+         Authorization: `Bearer ${token}`,
       },
-      httpsAgent: new https.Agent(
-                { 
-                  rejectUnauthorized: false
-                }
-      ),
-    });
+    })
     
     const result = await response.data
     return NextResponse.json(result)
---
+++ lib/Axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import https from 'https'
+import { cookies } from 'next/headers';
+const Axios = axios.create({
+    baseURL:'https://101.44.39.177:55000',
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+})
+ 
+
+export default Axios
