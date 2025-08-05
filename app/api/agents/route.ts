80495d88a2d0a163802b482599c310f41ee0fdb1
larbi-ishak <ishak.larbi@inttic.dz>
2025-08-05 13:09:12 +0100

    auth

--- app/api/agents/route.ts
+++ app/api/agents/route.ts
@@ -6,10 +6,11 @@ export async function GET() {
   try {
     const cookieStore = cookies();
     const token = (await cookieStore).get("token")?.value;
+    console.log("TOKEN ISJ: ", token);
     
     const response = await axios.get("https://101.44.39.177:55000/agents", {
       headers: {
-        Authorization: `Bearer eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3YXp1aCIsImF1ZCI6IldhenVoIEFQSSBSRVNUIiwibmJmIjoxNzU0Mzg5MDA0LCJleHAiOjE3NTQzODk5MDQsInN1YiI6IndhenVoIiwicnVuX2FzIjpmYWxzZSwicmJhY19yb2xlcyI6WzFdLCJyYmFjX21vZGUiOiJ3aGl0ZSJ9.AC4QoBZIDBoMoONb7zqp6T1JMdUWhs6NAuwQ4b958FDwkLX7tZeIbydNoIRntHroodGHgez-I0mavGiO5ibnbFAeAbWt9HcAKyAP-182SLVXmS2NY6ePD2CnTAOhqp5f4K0EcWgs_UcD9bkbgVZeFq4UVy1_KOcbckcxx_7PNWMJDFR5`,
+        Authorization: `Bearer ${token}`,
       },
       httpsAgent: new https.Agent(
                 { 
